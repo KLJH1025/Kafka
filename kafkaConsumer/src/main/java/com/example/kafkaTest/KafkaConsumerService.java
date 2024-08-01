bbdd53c5c4a6c38460eac28d4d5988dde325b2f5
이재혁 <dusk5588@naver.com>
2024-08-01 16:23:20 +0900

    Add Async

--- kafkaConsumer/src/main/java/com/example/kafkaTest/KafkaConsumerService.java
+++ kafkaConsumer/src/main/java/com/example/kafkaTest/KafkaConsumerService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -20,10 +21,22 @@ public class KafkaConsumerService {
     //groupId = "consumer_group01"
     @KafkaListener(topics = "email", groupId = "consumer_group01", containerFactory = "kafkaListenerContainerFactory")
     public void consumeEmailMessage(ConsumerRecord<String, NotificationMessageDto> messageDto) throws IOException {
+        long startTime = System.nanoTime();
+
         System.out.println("Success to reach Sender, Topic = Email");
+        System.out.printf("messageDto = %s\n", messageDto);
         NotificationMessageDto nmd = messageDto.value();
         emailService.sendMessage(nmd.receiverAddress(), nmd.title(), nmd.body());
         System.out.printf("Consumed Received Email Message : %s%n", nmd);
+
+        // 종료 시간 기록
+        long endTime = System.nanoTime();
+
+        // 실행 시간 계산 (나노초 단위)
+        long duration = endTime - startTime;
+
+        // 실행 시간 출력 (밀리초 단위로 변환)
+        System.out.println("Execution time: " + (duration) + " nanoseconds");
     }
 
 //    @KafkaListener(topics = "alarm", groupId = "consumer_group01")
